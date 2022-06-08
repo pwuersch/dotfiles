1a65e44420e9cfce3be01c214fea45e9cbe19668
pwuersch <49908921+pwuersch@users.noreply.github.com>
2022-06-09 01:06:42 +0200

    add cursor reset on nvim exit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,10 @@ require('plugins')
 require('opts')
 
 require('mappings')
+
+vim.cmd([[
+  augroup RestoreCursorShapeOnExit
+    autocmd!
+    autocmd VimLeave * set guicursor=a:ver20
+  augroup END
+]])
