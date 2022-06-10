0b8252fbbd83b1d254494b02c8d42a6d7907cb50
pwuersch <49908921+pwuersch@users.noreply.github.com>
2022-06-10 17:44:24 +0200

    configure mappings and telescope addons

--- .config/nvim/lua/config/dashboard.lua
+++ .config/nvim/lua/config/dashboard.lua
@@ -6,8 +6,13 @@ vim.g.dashboard_default_executive = 'telescope'
 --There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine’s resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!
 --]]}
 
+local function script_path()
+  local str = debug.getinfo(2, "S").source:sub(2)
+  return str:match("(.*/)")
+end
+
 vim.g.dashboard_preview_command = 'cat'
 vim.g.dashboard_preview_pipeline = ''
-vim.g.dashboard_preview_file = '~/.config/nvim/sunjon.cat'
+vim.g.dashboard_preview_file = vim.fn.resolve(script_path() .. '../../sunjon.cat')
 vim.g.dashboard_preview_file_height = 10
 vim.g.dashboard_preview_file_width = 70
--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,4 +1,16 @@
 local telescope = require('telescope')
+
+telescope.setup({
+  extensions = {
+    packer = {},
+    file_browser = {
+      theme = "ivy",
+      hijack_netrw = true,
+    },
+    lazygit = {},
+  }
+})
+
 telescope.load_extension('packer')
 telescope.load_extension('file_browser')
 telescope.load_extension('lazygit')
